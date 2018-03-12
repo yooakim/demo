7651be28f8291f63bc20773c2af899b11fbec569
Joakim Westin <joakim.westin@basefarm.com>
2018-03-12 14:45:16 +0100

    Minor updates

--- Basefarm.Demo.Web/Startup.cs
+++ Basefarm.Demo.Web/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Basefarm.Demo.Web
 {
@@ -25,6 +26,13 @@ namespace Basefarm.Demo.Web
         {
             services.AddDbContext<DiskContext>(opt=>opt.UseSqlServer(Configuration.GetConnectionString("Demo")));
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Disk API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,9 +47,32 @@ namespace Basefarm.Demo.Web
                 app.UseExceptionHandler("/Error");
             }
 
+            InitializeMigrations(app);
+
             app.UseStaticFiles();
 
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Demo API V1");
+            });
+
+
             app.UseMvc();
+        }
+
+        private static void InitializeMigrations(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                DiskContext dbContext = serviceScope.ServiceProvider.GetRequiredService<DiskContext>();
+                dbContext.Database.Migrate();
+
+                // TODO: Use dbContext if you want to do seeding etc.
+            }
         }
     }
 }
--- docs/PS-API-DEMO.md
+++ docs/PS-API-DEMO.md
@@ -1,2 +1,60 @@
-# PowerShell REST API demo
+## PowerShell REST API demo
+
+The ambition is to show how a simple REST API can be used from various places. The first example is PowerShell.
+
+
+### Adding disks (POST)
+
+
+```powershell
+$body = Get-CimInstance win32_logicaldisk | select Name,Description,SystemName,FreeSpace,Size,Compressed,DriveType,FileSystem,MaximumComponentLength,MediaType,SupportsDiskQuotas,SupportsFileBasedCompression,VolumeName,VolumeSerialNumber | ConvertTo-Json
+
+$headers = @{ "Content-Type"="application/json"}
+$result = Invoke-RestMethod -Method POST -Uri http://localhost:61170/api/LogicalDisks -Body $body -Headers $headers | ConvertTo-Json
+
+```
+
+
+### Updating disks (PUT)
+
+```powershell
+$body = @"
+{
+  "id": 2,
+  "name": "C:",
+  "description": "Local Fixed Disk",
+  "systemName": "BFW-5KP0H72",
+  "freeSpace": 37952184320,
+  "size": 138438242304,
+  "compressed": true,
+  "driveType": 3,
+  "fileSystem": "NTFS",
+  "maximumComponentLength": "255",
+  "mediaType": 12,
+  "supportsDiskQuotas": false,
+  "supportsFileBasedCompression": true,
+  "volumeDirty": false,
+  "volumeName": "Windows",
+  "volumeSerialNumber": "F00F7CC4",
+  "used": 100486057984
+}
+"@ 
+
+$headers = @{ "Content-Type"="application/json"}
+$result = Invoke-RestMethod -Method PUT -Uri http://localhost:61170/api/LogicalDisks/2 -Body $body -Headers $headers
+```
+
+###  Getting datat (Get)
+
+```powershell
+$headers = @{ "Content-Type"="application/json"}
+$disks = Invoke-RestMethod -Method GET -Uri http://localhost:61170/api/LogicalDisks -Headers $headers -DisableKeepAlive
+$disks | Format-Table -AutoSize
+```
+
+
+
+
+
+
 
