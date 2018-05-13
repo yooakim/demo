8834a9fc680ec90ab725db7cf0d9e353a219e1d0
Joakim Westin <yooakim@gmail.com>
2018-05-13 21:32:18 +0200

    Fixes

---
+++ Basefarm.Demo.Web/Controllers/DrivesController.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Basefarm.Demo.Web.Data;
+using Basefarm.Demo.Web.Entities;
+
+namespace Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Drives")]
+    public class DrivesController : Controller
+    {
+        private readonly DiskContext _context;
+
+        public DrivesController(DiskContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Drives
+        [HttpGet]
+        public IEnumerable<PSDrive> GetPSDrives()
+        {
+            return _context.PSDrives;
+        }
+
+        // GET: api/Drives/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPSDriveInfo([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pSDriveInfo = await _context.PSDrives.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (pSDriveInfo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pSDriveInfo);
+        }
+
+        // PUT: api/Drives/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPSDriveInfo([FromRoute] int id, [FromBody] PSDrive pSDriveInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pSDriveInfo.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pSDriveInfo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PSDriveInfoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Drives
+        [HttpPost]
+        public async Task<IActionResult> PostPSDriveInfo([FromBody] PSDrive pSDriveInfo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.PSDrives.Add(pSDriveInfo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPSDriveInfo", new { id = pSDriveInfo.Id }, pSDriveInfo);
+        }
+
+        // DELETE: api/Drives/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePSDriveInfo([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var pSDriveInfo = await _context.PSDrives.SingleOrDefaultAsync(m => m.Id == id);
+            if (pSDriveInfo == null)
+            {
+                return NotFound();
+            }
+
+            _context.PSDrives.Remove(pSDriveInfo);
+            await _context.SaveChangesAsync();
+
+            return Ok(pSDriveInfo);
+        }
+
+        private bool PSDriveInfoExists(int id)
+        {
+            return _context.PSDrives.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Basefarm.Demo.Web/Migrations/20180328063155_Initial.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Web.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "LogicalDisks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Compressed = table.Column<bool>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    DriveType = table.Column<int>(nullable: false),
+                    FileSystem = table.Column<string>(nullable: true),
+                    FreeSpace = table.Column<long>(nullable: false),
+                    MaximumComponentLength = table.Column<int>(nullable: false),
+                    MediaType = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Size = table.Column<long>(nullable: false),
+                    SupportsDiskQuotas = table.Column<bool>(nullable: false),
+                    SupportsFileBasedCompression = table.Column<bool>(nullable: false),
+                    SystemName = table.Column<string>(nullable: true),
+                    VolumeDirty = table.Column<bool>(nullable: false),
+                    VolumeName = table.Column<string>(nullable: true),
+                    VolumeSerialNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LogicalDisks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PSDrives",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Free = table.Column<long>(nullable: false),
+                    HostName = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Root = table.Column<string>(nullable: true),
+                    Used = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PSDrives", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LogicalDisks");
+
+            migrationBuilder.DropTable(
+                name: "PSDrives");
+        }
+    }
+}
