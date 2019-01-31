f353d241afe5fa49937df6a607f3d5dfe36d3342
Joakim Westin <yooakim@gmail.com>
2019-01-31 20:17:27 +0100

    Switched to using octo from dotnet instead of exe

--- tools/build-and-push-to-octopus.ps1
+++ tools/build-and-push-to-octopus.ps1
@@ -1,10 +1,10 @@
 
 
 dotnet publish Basefarm.Demo.Web --configuration Release  --runtime win-x64
-octo pack --id Basefarm.Demo.Web --basePath .\Basefarm.Demo.Web\bin\Release\netcoreapp2.0\publish --format=zip --outFolder=.\packaged
+dotnet octo pack --id Basefarm.Demo.Web --basePath .\Basefarm.Demo.Web\bin\Release\netcoreapp2.0\publish --format=zip --outFolder=.\packaged
 
 # Find out the name of the most recent file
 $packageName = (get-childitem .\packaged\ -filter "*.zip" | Sort-Object LastWriteTime -Descending | Select-Object -First 1).Name
 
-octo push --server=https://octopus.skl.se  --apiKey="$env:OCTOPUS_API_KEY" --ignoreSslErrors --package=.\packaged\$packageName
+dotnet octo push --server=https://octopus.skl.se  --apiKey="$env:OCTOPUS_API_KEY" --ignoreSslErrors --package=.\packaged\$packageName
 
